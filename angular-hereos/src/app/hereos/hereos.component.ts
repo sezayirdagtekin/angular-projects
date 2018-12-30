358db47a4e085a4d3bb335952eb4a80dbacd475b
sezayirdagtekin <sezayir@hotmail.com>
2018-12-30 16:57:14 +0300

    list added

--- angular-hereos/src/app/hereos/hereos.component.ts
+++ angular-hereos/src/app/hereos/hereos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
+import { HEADER_OFFSET } from '@angular/core/src/render3/interfaces/view';
 
 
 @Component({
@@ -9,15 +11,25 @@ import { Hero } from '../hero';
 })
 export class HereosComponent implements OnInit {
 
+selectedHero:Hero;
 
   hero: Hero = {
     id: 5,
     name:'Windstorm'
   };
   
+  heroes = HEROES;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
+
+  onSelect(hero:Hero) :void{
+    this.selectedHero=hero;
+  }
+
 
 }
