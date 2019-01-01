109a8b72554c5a86d634bd24cdafe475c69f1f15
sezayirdagtekin <sezayir@hotmail.com>
2019-01-01 13:41:22 +0300

    router added

--- angular-hereos/src/app/app.module.ts
+++ angular-hereos/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HereosComponent } from './hereos/hereos.component';
+
 import { MessageComponent } from './message/message.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './hereos/hereos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HereosComponent,
-    MessageComponent
+    HeroesComponent,
+    MessageComponent,
+    DashboardComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
--- angular-hereos/src/app/hero.service.ts
+++ angular-hereos/src/app/hero.service.ts
@@ -1,22 +1,26 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessagesService } from './messages.service';
 
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
   constructor(private messageService: MessagesService) { }
 
   getHeroes(): Observable<Hero[]> {
-
-    this.messageService.add("HeroService: fetched heroes");
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
-
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
